47a6be849bf8bb3dbbf47c18ab085a62ef574ca5
nao3301 <nao.lupa@gmail.com>
2018-04-06 16:24:41 +0200

    reuse splash screen

--- main.js
+++ main.js
@@ -25,7 +25,28 @@ function createWindow () {
 	});
 }
 
-app.on('ready', createWindow);
+//reuse
+app.on('ready', () => {
+  // create main browser window
+  mainWindow = new BrowserWindow({
+      titleBarStyle: 'hidden',
+      width: 1920,
+      height: 1080,
+      show: false // don't show the main window
+  });
+  // create a new `splash`-Window 
+  splash = new BrowserWindow({width: 810, height: 610, transparent: true, frame: false, alwaysOnTop: true});
+  splash.loadURL(`file://${__dirname}/splash.html`);
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+  
+  // if main window is ready to show, then destroy the splash window and show up the main window
+  mainWindow.once('ready-to-show', () => {
+    splash.destroy();
+    mainWindow.show();
+  });
+});
+
+//reuse//
 
 // quit
 app.on('window-all-closed', () => {
